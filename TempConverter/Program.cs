705593cd361315b7a704248bacf5c35b85d884d7
Luke Blondin <lblondin747@gmail.com>
2021-07-29 14:57:40 -0500

    i

---
+++ TempConverter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TempConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TempConverter test = new TempConverter();
+
+            Console.WriteLine($"89 degrees fahrenheit is {TempConverter.FahrenheitToCelsius(89)} degrees celsius");
+            Console.WriteLine($"89 degrees celsius is {TempConverter.CelsiusToFahrenheit(89)} degrees fahrenheit");
+
+        }
+    }
+}
---
+++ TempConverter/TempConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace TempConverter
+{
+    public class TempConverter
+    {
+        public TempConverter()
+        {
+        }
+        public static double CelsiusToFahrenheit(double input)
+        {
+            return (input * 9 / 5) + 32;
+        }
+        public static double FahrenheitToCelsius(double input)
+        {
+            return (input - 32) * 5 / 9;
+        }
+
+    }
+}
